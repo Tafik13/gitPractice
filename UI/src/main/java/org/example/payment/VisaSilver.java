0edd125a2ff8ef52ed0441c0cfa66e905f6917bd
tahir <tafik13.01.90@gmail.com>
2023-10-12 09:31:13 +0600

    addedPackageToSilverCard

--- UI/src/main/java/org/example/payment/VisaSilver.java
+++ UI/src/main/java/org/example/payment/VisaSilver.java
@@ -1,1 +1,8 @@
-public class VisaSilver
+package org.example.payment;
+
+public class VisaSilver{
+    public static void main(String[] args) {
+        System.out.println("Visa Platinum");
+    }
+}
+
